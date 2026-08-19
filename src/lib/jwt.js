e77aec218430df45df66e2cf8dc8e9a2e2de05ec
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-08-19 16:44:57 +0700

    fix: fixing expired token jwt

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "1d";
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "15m";
 
 if (!JWT_SECRET) {
   throw new Error(
